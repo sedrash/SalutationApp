0a3c65df7e5b4f2d2f4fd952045a55e7285f3456
Marten Tschatschanidse <mtschatscha.gre2027@esaip.org>
2025-10-31 13:51:16 +0100

    commit Salut de Marten

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,6 +1,6 @@
 public class Salutation {
     public static void main(String[] args) {
         System.out.println("Hello, user!");
-        System.out.println("Bonjour de Sedra !");
+        System.out.println("Bonjour de Marten !");
     }
 }
